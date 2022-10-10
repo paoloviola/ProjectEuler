cac0ca965727c4d6891c1ec01b2c1ab4a0cf384f
Paolo V <paolo.viola@cgi.com>
2022-10-10 12:35:32 +0200

    solved problem 37

---
+++ ProjectEuler/Chapter4/Problem37.cs
@@ -0,0 +1,67 @@
+﻿using ProjectEuler.Common;
+using ProjectEuler.Common.Problem;
+using System.Text;
+
+namespace ProjectEuler.Chapter4
+{
+    [ProblemSolved]
+    internal class Problem37 : IProblem
+    {
+        public string Name => "Truncatable primes";
+        public string TestResult => "True";
+        public string SolveResult => "748317";
+
+        public string Test()
+        {
+            return IsTruncatablePrime(3797).ToString();
+        }
+
+        public string Solve()
+        {
+            return GetSumOfTruncatablePrimes().ToString();
+        }
+
+        private static bool IsTruncatablePrime(int num)
+        {
+            var sb = new StringBuilder(num.ToString());
+            do
+            {
+                if (!int.Parse(sb.ToString()).IsPrime())
+                {
+                    return false;
+                }
+                sb.Remove(0, 1);
+            }
+            while (sb.Length > 0);
+
+            sb = new StringBuilder(num.ToString());
+            do
+            {
+                if (!int.Parse(sb.ToString()).IsPrime())
+                {
+                    return false;
+                }
+                sb.Remove(sb.Length - 1, 1);
+            }
+            while (sb.Length > 0);
+
+            return true;
+        }
+
+        private static long GetSumOfTruncatablePrimes()
+        {
+            int found = 0;
+
+            long sum = 0;
+            for(int i = 10; found < 11; i++)
+            {
+                if(IsTruncatablePrime(i))
+                {
+                    sum += i;
+                    found++;
+                }
+            }
+            return sum;
+        }
+    }
+}
