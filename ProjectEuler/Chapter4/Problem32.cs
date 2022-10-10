05ed8f0c761baf82f355acbf2f2c945f25cfa064
Paolo V <paolo.viola@cgi.com>
2022-10-10 10:07:11 +0200

    solved problem 32

--- ProjectEuler/Chapter4/Problem32.cs
+++ ProjectEuler/Chapter4/Problem32.cs
@@ -3,11 +3,12 @@ using System.Text;
 
 namespace ProjectEuler.Chapter4
 {
+    [ProblemSolved]
     internal class Problem32 : IProblem
     {
         public string Name => "Pandigital products";
         public string TestResult => "True";
-        public string SolveResult => "268218";
+        public string SolveResult => "45228";
 
         public string Test()
         {
@@ -21,6 +22,11 @@ namespace ProjectEuler.Chapter4
 
         private static bool IsPandigital(string str, int cap)
         {
+            if(str.Length != cap || str.Contains('0'))
+            {
+                return false;
+            }
+
             for(int i = 1; i <= cap; i++)
             {
                 if(str.Count(c => c.ToString() == i.ToString()) != 1)
@@ -31,39 +37,13 @@ namespace ProjectEuler.Chapter4
             return true;
         }
 
-        private static bool ContainsDuplicates(string str)
-        {
-            for(int i = 0; i < str.Length; i++)
-            {
-                for(int digit = 0; digit <= 9; digit++)
-                {
-                    if (str.Count(c => c.ToString() == digit.ToString()) > 1)
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
         private static long SumOfPandigitalProducts(int cap)
         {
             var sums = new HashSet<long>();
-            for(int a = 10; a < 1e+3; a++)
+            for(int a = 1; a < 100; a++)
             {
-                if(ContainsDuplicates(a.ToString()))
+                for (int b = 1; b < 10000; b++)
                 {
-                    continue;
-                }
-
-                for (int b = 100; b < 1e+4; b++)
-                {
-                    if (ContainsDuplicates(a.ToString() + b.ToString()))
-                    {
-                        continue;
-                    }
-
                     var str = new StringBuilder();
                     str.Append(a).Append(b).Append(a * b);
                     if(IsPandigital(str.ToString(), cap) && !sums.Contains(a * b))
